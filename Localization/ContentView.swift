09d22c7bc30a71e919ea45beedbfeb989e664d3b
Mahdi <97763355+MHanifeh@users.noreply.github.com>
2022-06-18 12:27:44 +0430

    Initial Commit

---
+++ Localization/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Localization
+//
+//  Created by MahdiHanifeh on 3/28/1401 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Localization/LocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationApp.swift
+//  Localization
+//
+//  Created by MahdiHanifeh on 3/28/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
